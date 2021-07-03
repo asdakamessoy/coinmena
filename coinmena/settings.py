70883351eb87373fa23ff5cda3b87d3c48757d34
Ashad Chaudhary <ashad,chaudhary@democrance.com>
2021-07-03 13:47:48 +0400

    secret key removed

--- coinmena/settings.py
+++ coinmena/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "cj1jzsprq61f@%+fmc5#%%ie%&^63mi4fqfz9-+@#a2t59^+0^"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
